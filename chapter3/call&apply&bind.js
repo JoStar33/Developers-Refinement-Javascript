e9ec8c864c0d4e05d0d964470c1332b515341388
[아이엔소프트] 황호세 <hshwang@in-soft.co.kr>
2023-04-10 11:14:38 +0900

    feat: call, apply, bind 연습

---
+++ chapter3/call&apply&bind.js
@@ -0,0 +1,17 @@
+function bindFunction() {
+  return this;
+}
+/*
+[ 1, 2, 3 ]
+[Number: 1]
+*/
+console.log(bindFunction.apply([1, 2, 3]));
+console.log(bindFunction.call(1, 2, 3));
+
+/*
+바인드는 다음과 같이 함수를 실행시키지 않기때문에 ()를 붙여줘야한다.
+[Function: bound bindFunction]
+[Number: 1]
+*/
+console.log(bindFunction.bind(1));
+console.log(bindFunction.bind(1)());
