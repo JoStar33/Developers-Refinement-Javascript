b916814b19d8ce3477f10cd7cda832bbad3eab3f
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-04-13 12:43:06 +0900

    feat: 스트링 프로토타입 함수들 DeepDive 3

--- chapter7/stringPrototype2.js
+++ chapter7/stringPrototype2.js
@@ -12,3 +12,64 @@ const str10 = "first third third fourth";
 const str11 = "dog dog dog dog dog";
 //cat cat cat cat cat
 //console.log(str11.replaceAll("dog", "cat"));
+
+
+
+//String.prototype.search()
+//정규 표현식에 처음으로 매칭되는 값의 인덱스를 반환한다.
+const str12 = "hello, funny dog"
+// const regex = /dog/i;
+// 13
+// console.log(str12.search(regex));
+
+
+
+
+//String.prototype.slice()
+const str13 = "the world";
+// console.log(str13.slice(0, 3)); // the
+// console.log(str13.slice(4)); // world
+
+
+
+
+//String.prototype.split()
+//특정 단어가 들어올때 그 부분을 때어서 배열로 저장하는 방식
+const str14 = "dog dog dog dog dog cat cat cat cat pig pig";
+const words = str14.split(' ');
+//console.log(words[8]);
+
+
+
+
+//String.prototype.toLowerCase()
+//전부 소문자로
+const kor = "HELLO KOREA";
+//console.log(kor.toLowerCase()); 
+
+
+
+
+//String.prototype.toLowerCase()
+//전부 대문자로
+const kor2 = "hello korea";
+//console.log(kor.toUpperCase());
+
+
+
+
+//String.prototype.toString()
+//객체형태의 스트링을 문자열로 반환.
+const stringObj = new String('foo');
+console.log(stringObj);
+console.log(stringObj.toString());
+
+
+
+
+
+//String.prototype.trim()
+//공백을 제거한다.
+const trimString = "yyyy yyy yy yy yy yyyy                "
+console.log(trimString);
+console.log(trimString.trim());
