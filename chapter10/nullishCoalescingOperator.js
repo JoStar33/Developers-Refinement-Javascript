42a88a880ca7088dd811eecaedef66c522a6ebd3
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-04-18 12:50:36 +0900

    feat: Nullish Coalescing Operator

---
+++ chapter10/nullishCoalescingOperator.js
@@ -0,0 +1,8 @@
+let serverData = undefined;
+serverData ??= 20;
+console.log(serverData);
+
+
+const response = undefined;
+const showData = response || "this is undefined!";
+console.log(showData);
