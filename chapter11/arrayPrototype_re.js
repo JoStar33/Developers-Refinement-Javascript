86fe6fe7dbdfddde1325441dfe6a3115447f6300
HoCG <rhwkd5012@naver.com>
2023-04-23 22:36:49 +0900

    feat: 이벤트 루프 복습

---
+++ chapter11/arrayPrototype_re.js
@@ -0,0 +1,19 @@
+const arrFill = [
+  {
+    id: 23,
+    name: "james"
+  }, {
+    id: 24,
+    name: "park"
+  }, {
+    id: 25,
+    name: "lonoo"
+  }, {
+    id: 26,
+    name: "koko"
+  }, {
+    id: 27,
+    name: "pin"
+  }
+];
+console.log(arrFill.fill({id: 0, name: ""}));
